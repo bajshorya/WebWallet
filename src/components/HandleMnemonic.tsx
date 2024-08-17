c3fe9d56650c3186cb93050421679630f60b6222
SHORYA BAJ <jain.shorya11@gmail.com>
2024-08-17 20:55:18 +0530

    checkpoint

--- src/components/HandleMnemonic.tsx
+++ src/components/HandleMnemonic.tsx
@@ -2,22 +2,50 @@ import React, { useState } from "react";
 import Generate from "./Generate";
 import GenerateWallets from "./GenerateWallets";
 import GenerateEthWallets from "./GenerateEthWallets";
+import { Button } from "./ui/button";
 
 const HandleMnemonic = () => {
   const [mnemonic, setMnemonic] = useState("");
+  const [selectedWallet, setSelectedWallet] = useState<"solana" | "ethereum">(
+    "solana"
+  );
+
   const handleGenerateMnemonic = (newMnemonic: string) => {
     setMnemonic(newMnemonic);
   };
+
+  const handleSelectWallet = (walletType: "solana" | "ethereum") => {
+    setSelectedWallet(walletType);
+  };
+
   return (
     <div>
       <Generate
         onGenerateMnemonic={handleGenerateMnemonic}
         mnemonic={mnemonic}
       />
-
+      <div className="flex gap-3 justify-center m-5">
+        <Button
+          variant={selectedWallet === "solana" ? "default" : "secondary"}
+          onClick={() => handleSelectWallet("solana")}
+          className={selectedWallet === "solana" ? "border " : ""}
+        >
+          Solana
+        </Button>
+        <Button
+          variant={selectedWallet === "ethereum" ? "default" : "secondary"}
+          onClick={() => handleSelectWallet("ethereum")}
+          className={selectedWallet === "ethereum" ? "border " : ""}
+        >
+          Ethereum
+        </Button>
+      </div>
       <div className="flex gap-3 justify-around">
-        <GenerateWallets mnemonic={mnemonic} />
-        <GenerateEthWallets mnemonic={mnemonic} />
+        {selectedWallet === "solana" ? (
+          <GenerateWallets mnemonic={mnemonic} />
+        ) : (
+          <GenerateEthWallets mnemonic={mnemonic} />
+        )}
       </div>
     </div>
   );
